0a47557e8d3cc1010fb9169284f8b6eef671d43f
asRagi <ragi-justaway@hotmail.co.jp>
2020-11-14 22:57:27 +0900

    Add ActorId column

--- TestDomain/Entities/Battle/TestActor.cs
+++ TestDomain/Entities/Battle/TestActor.cs
@@ -1,5 +1,6 @@
 using BuddyDomain.Entities.Battle;
 using BuddyDomain.ValueObjects.Battle;
+using BuddyDomain.ValueObjects.Battle.Actor;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestDomain.Entities.Battle
@@ -22,12 +23,13 @@ namespace TestDomain.Entities.Battle
 
         private static Actor CreateActor()
         {
+            var actorId = new ActorId("TestActor");
             var health = new HealthPoint(100);
             var max = new MaxHealthPoint(health);
             var attack = new Attack(50);
             var speed = new Speed(50);
             var magic = new Magic(50);
-            return new Actor(max, health, attack, speed, magic);
+            return new Actor(actorId, max, health, attack, speed, magic);
         }
     }
 }
